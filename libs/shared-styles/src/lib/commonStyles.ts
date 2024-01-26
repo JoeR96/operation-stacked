1875674270164f950d0b2d07453d51bc501228a9
Joe Richardson <joeyrichardson96@gmail.com>
2024-01-26 16:06:44 +0000

    Add shared-styles library and update dependencies
    This commit introduces the shared-styles library and updates several project dependencies. The shared-styles library includes common styles used across the project. Additionally, new dependencies and scripts related to Jest and Babel have been added to package.json to enhance testing and transpiling capabilities.

---
+++ libs/shared-styles/src/lib/commonStyles.ts
@@ -0,0 +1,11 @@
+import { theme } from './theme';
+
+export const commonStyles = {
+  button: {
+    backgroundColor: theme.colors.primary,
+    color: 'white',
+  },
+  text: {
+    color: theme.colors.text,
+  },
+};
---
+++ libs/shared-styles/src/lib/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  colors: {
+    primary: '#FF6000',
+    secondary: '#FFA559',
+    background: '#454545',
+    text: '#FFFFFF',
+    accent: '#FFE6C7',
+  },
+};
